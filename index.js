b706eedb2b2cbecdaa069493337b5a7e63292dce
Ekram Hossain <ekramhossain117@gmail.com>
2023-05-04 12:13:20 +0600

    added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors=require("cors");
 const port = process.env.PORT || 5000;
 const chef=require('./data/chef.json')
+const items=require('./data/items.json')
 app.use(cors());
 app.get('/', (req, res) => {
       res.send('Recipe Hunter is running')
@@ -11,6 +12,13 @@ app.get('/chef',(req,res) =>{
       res.send(chef);
 
 })
+app.get('/items/:id',(req,res)=>{
+      const _id=req.params.id;
+      // console.log(id);
+      const newItems=items.filter((item) =>item.id==_id)
+      res.send(newItems);
+})
+
 app.listen(port, () => {
       console.log(`recipe hunter is  on port ${port}`)
     })
